f37f1cc0a95f448ef1491e221dfbbef8781fd6bd
SmeeBear <soheron@gmail.com>
2019-10-09 18:30:04 -0500

    initial

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://oherons:MNtoTN1997%21@mern-shopping-nkaq3.mongodb.net/test?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//bodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config 
+const db = require('./config/keys').mongoURI
+
+const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Connect to Mongo
+
+mongoose.connect(db, options=opts)
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch(err => console.log(err))
+
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`Server started on port ${port}`))
